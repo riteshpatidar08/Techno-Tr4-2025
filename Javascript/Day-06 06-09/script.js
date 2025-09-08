585e70f6fa02d43938c18a8ebd8e388cd76ef584
ritesh <riteshpatidar088@gmail.com>
2025-09-08 13:39:20 +0530

    added destructuring task

---
+++ Javascript/Day-06 06-09/script.js
@@ -0,0 +1,54 @@
+// //NOTE DESTRUCTURING *****
+// //using destructuring we can directly save value of array to the variables ;
+// const num = [1,2,3,4];
+// // const a = num[0] ;
+// // const b = num[1];
+// // const [a,b,c,d] = num ;
+// // console.log(a,b,c,d) ;
+
+// //USE CASE SKIPPING A VALUE
+// const [a,b,,d] = num ;
+//  console.log(a,b,d) ;
+
+//  //NOTE default values :
+//  const evenNumber = [2,3,4,8];
+//  const [x,y,z,p=6] = evenNumber;
+//  console.log(p)
+
+const nestedArr = [1, 2, [3, 4, [5, 6]]];
+const [a, b, [c, d, [e, f]]] = nestedArr;
+console.log(c);
+
+//NOTE OBJECT DESTUCTURING :
+
+const car = {
+  brand: 'honda',
+  color: 'white',
+  engineSpec: '2.2l',
+};
+const { color, brand, engineSpec = '1500cc' } = car;
+console.log(brand, color, engineSpec);
+
+const detail = {
+  firstName: 'test',
+  address: {
+    city: 'UDAIPUR',
+  },
+};
+//Note changing default key name and nested
+const {
+  firstName: fName,
+  address: { city },
+} = detail;
+console.log(fName, city);
+
+const userData = [
+  { id: 1, name: 'test1', email: 'test1@gmail.com', roles: ['admin', 'editor'] },
+  { id: 2, name: 'test2', email: 'test2@gmail.com', roles: ['student', 'admin'] },
+  {
+    id: 3,
+    name: 'test3',
+    email: 'test3@gmail.com',
+    roles: ['admin', 'superadmin'],
+  },
+];
