c7787218249049b1b3c03f906a5a602b3dde97ad
ritesh <riteshpatidar088@gmail.com>
2025-09-10 15:09:44 +0530

    updated till insertbefore

--- Javascript/Day08 10-09/script.js
+++ Javascript/Day08 10-09/script.js
@@ -48,19 +48,54 @@
 // container.innerHTML = `<h1>TEST</h1><p>test@gmail.com</p>`;
 
 //NOTE UPDATE ANCHOR TAG HREF
-const link = document.getElementById("link");
-link.href ='https://www.blinkit.in';
+const link = document.getElementById('link');
+link.href = 'https://www.blinkit.in';
 link.innerText = 'BLINKIT';
 
 //NOTE UPDATING IMAGE SRC
 const image = document.getElementById('img');
-console.dir(image)
-image.src = 'https://placehold.co/600x400'
-
+console.dir(image);
+image.src = 'https://placehold.co/600x400';
 
 const h2 = document.getElementById('heading_second');
-console.log(h2.className)
+console.log(h2.className);
 // h2.className = 'test'
 console.log(h2.classList);
-h2.classList.add('test')
-h2.classList.remove('head2')
+h2.classList.add('test');
+h2.classList.remove('head2');
+
+//NOTE GETTING AND SETTING ATTRIBUTES ON AN ELEMENT  :
+console.log(h2.getAttribute('id')); //use to get the value of specific attribute
+
+//NOTE SETTING A NEW ATTRIBUTE ON AN ELEMENT
+h2.setAttribute('title', 'this is heading');
+h2.setAttribute('style', 'background-color:red');
+
+//applying style
+h2.style.color = 'orange';
+h2.style.fontSize = '60px';
+
+//NOTE CREATING NEW ELEMENTS USING JS
+
+const b = document.createElement('b');
+console.log(b);
+//text fill
+b.textContent = 'bold';
+document.body.prepend(b);
+//NOTE append add in the  end //prepend add in the start
+
+//create a list and insert 3 li in the list using js
+
+const list = document.createElement('ul');
+console.log(list);
+list.innerHTML = '<li>item1</li><li id=item_2>item2</li><li>item3</li>';
+
+document.body.append(list);
+
+//next insert new li between item1 and item2
+
+//insertBefore(newElement , referenceElement) apply on parent
+const li = document.createElement('li');
+const referenecLi = document.getElementById('item_2');
+li.textContent = 'new added list item';
+list.insertBefore(li, referenecLi);
