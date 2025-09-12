d9b50254cd235090b46a1b48992729a03d3edc1e
ritesh <riteshpatidar088@gmail.com>
2025-09-12 13:23:55 +0530

    added api calling

---
+++ Javascript/Day10 12-09/script.js
@@ -0,0 +1,16 @@
+//user enter movie name into the input field;
+//when you click on the search button , your form going to be submit ;
+
+//task 1 select the form and input field
+const form = document.getElementById('form');
+const inputSearch = document.getElementById('search');
+
+//add event on the form and get the value from the field
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchQuery = inputSearch.value;
+  console.log(searchQuery);
+  fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=b0eec689`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
