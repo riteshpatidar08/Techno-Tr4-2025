4241b65960e466cc5456e42d940740c576fd24f1
ritesh <riteshpatidar088@gmail.com>
2025-09-12 13:27:06 +0530

    added api calling

--- Javascript/Day10 12-09/script.js
+++ Javascript/Day10 12-09/script.js
@@ -4,13 +4,17 @@
 //task 1 select the form and input field
 const form = document.getElementById('form');
 const inputSearch = document.getElementById('search');
-
+const btn = document.getElementById('btn')
 //add event on the form and get the value from the field
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const searchQuery = inputSearch.value;
   console.log(searchQuery);
+btn.textContent = 'Searching....'
   fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=b0eec689`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data)).catch(err=> conosle.log(err))
+    .finally(()=>{
+        btn.textContent = 'Search'
+    })
 });
