159d361d158092cb8c01fa318348d2ced615484d
ritesh <riteshpatidar088@gmail.com>
2025-09-15 15:53:08 +0530

    update till modules

--- modules/script.js
+++ modules/script.js
@@ -1,8 +1,16 @@
-//NOTE MODULES 
+//NOTE MODULES
 //NOTE NAMED IMPORTS
-import {pi,firstname} from './utils.js'
-console.log(pi)
-console.log(firstname)
+import { pi, firstname as fname, product, evenNumber } from './utils.js';
+//defaul imports
+import demo1 from './utils.js'
+console.log(demo1)
+//Note pattern 2 for import named exports save all in one variable
+import * as utils from './utils.js';
+console.log(utils.evenNumber);
+console.log(pi);
+console.log(fname);
+console.log(evenNumber);
+console.log(product);
 
 // const college = 'techno' ;
 // console.log(college)
--- modules/utils.js
+++ modules/utils.js
@@ -4,12 +4,20 @@ export const pi = 3.14 ;
 export const firstname = 'test'
 
 //NOTE PATTERN SECOND FOR EXPORT NAMED EXPORTS
-
+const product = 'phone';
+const evenNumber = 2;
+export {product,evenNumber}
 
 // const college = 'TECHNO2';
 // console.log(college)
 
+//NOTE DEFAULT EXPORTS
+// function demo(){
+//     console.log('demmoooo..')
+// }
+// export default demo ;
 
+export default 'test';
 //exports => named export , default export
 
 // {
